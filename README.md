f219fb02be8734e3c206b90e45132c7b5efe3349
nakedible <naked@iki.fi>
2013-10-24 22:28:18 +0300

    Update both README and trackcensor template.

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
 trackcensor-exercise
 ====================
 
-A training exercise for financial card magnetic track censoring
+A training exercise for financial card magnetic track censoring.
+
+Provide an implementation the function in `trackcensor.c`. The
+function should both censor and encrypt financial card magnetic track
+data. The censoring should be done as required by PCI DSS 2.0 and the
+encryption should be done directly to an RSA public key.
+
+Requirements
+------------
+
+- Function must never output any cleartext which would be a PCI DSS
+  violation to store without encryption.
+
+- Function must output a PCI DSS compliant censoring of track 2 data
+  and PAN separately.
+
+- Function must output an RSA encrypted version of track 2 data and
+  PAN separately.
+
+- Function returns 0 on success or an error code on error.
+
+- BONUS: Implement full functionality also for track 1.
+
+Notes
+-----
+
+- RSA public key is at least 2048 bits in length.
+
+- Input track data is in binary without parity bits. This means that
+  there are 4 bits per symbol for track 2 and 6 bits per symbol for
+  track 1.
+
+- Output track data should be in ASCII format as used commonly in
+  banking. This means that every symbol in track 1 should have 0x20
+  added to it and every symbol in track 2 should have 0x30 added to
+  it obtain an ascii representation of the track.
+
+- OpenSSL `RSA_public_encrypt` should be used to encrypt the track
+  data directly to a given public key.
--- trackcensor.c
+++ trackcensor.c
@@ -1,19 +1,42 @@
 #include <openssl/rsa.h>
 
-void process_track(unsigned char *buffer,
-                   size_t length,
-                   int track_number,
-                   unsigned char *censored_track,
-                   size_t *censored_track_length,
-                   size_t censored_track_size,
-                   unsigned char *censored_pan,
-                   size_t *censored_pan_length,
-                   size_t censored_pan_size,
-                   unsigned char *encrypted_track,
-                   size_t *encrypted_track_length,
-                   size_t encrypted_track_size,
-                   unsigned char *out_encrypted_pan,
-                   size_t *out_encrypted_pan_length,
-                   size_t encrypted_track_size)
+/* typedef struct rsa_st RSA; */
+
+/** Process a magnetic track, producing both censored and encrypted
+ *  versions of full track data and PAN separately.
+ *
+ *  \param buffer binary track data to process
+ *  \param length length of the binary track data in bytes
+ *  \param censored_track censored track data output buffer
+ *  \param censored_track_length length of the censored track data written
+ *  \param censored_track_size size of the censored track data output buffer in bytes
+ *  \param censored_pan censored PAN output buffer
+ *  \param censored_pan_length length of the censored PAN written
+ *  \param censored_pan_size size of the censored PAN output buffer in bytes
+ *  \param public_key RSA public key to encrypt to
+ *  \param encrypted_track encrypted track data output buffer
+ *  \param encrypted_track_length length of the encrypted track data written
+ *  \param encrypted_track_size size of the encrypted track data output buffer in bytes
+ *  \param encrypted_pan encrypted PAN output buffer
+ *  \param encrypted_pan_length length of the encrypted PAN written
+ *  \param encrypted_pan_size size of the encrypted PAN output buffer in bytes
+ *  \return error code
+ */
+int process_track(const unsigned char *buffer,
+                  size_t length,
+                  int track_number,
+                  unsigned char *censored_track,
+                  size_t *censored_track_length,
+                  size_t censored_track_size,
+                  unsigned char *censored_pan,
+                  size_t *censored_pan_length,
+                  size_t censored_pan_size,
+                  RSA *public_key,
+                  unsigned char *encrypted_track,
+                  size_t *encrypted_track_length,
+                  size_t encrypted_track_size,
+                  unsigned char *out_encrypted_pan,
+                  size_t *out_encrypted_pan_length,
+                  size_t encrypted_track_size)
 {
 }
