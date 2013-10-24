2510a1105efb43b51d11712abf2cea28da229c36
Nuutti Kotivuori <naked@iki.fi>
2013-10-24 21:56:21 +0300

    Initial version of trackcensor.

---
+++ trackcensor.c
@@ -0,0 +1,19 @@
+#include <openssl/rsa.h>
+
+void process_track(unsigned char *buffer,
+                   size_t length,
+                   int track_number,
+                   unsigned char *censored_track,
+                   size_t *censored_track_length,
+                   size_t censored_track_size,
+                   unsigned char *censored_pan,
+                   size_t *censored_pan_length,
+                   size_t censored_pan_size,
+                   unsigned char *encrypted_track,
+                   size_t *encrypted_track_length,
+                   size_t encrypted_track_size,
+                   unsigned char *out_encrypted_pan,
+                   size_t *out_encrypted_pan_length,
+                   size_t encrypted_track_size)
+{
+}
